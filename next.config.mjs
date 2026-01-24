e807ab1b8a6c6429566b3d15702f143df9102aa2
studiobsquare <info.studiobsquare@gmail.com>
2026-01-24 19:06:10 +0530

    feat: add image remote patterns configuration to Next.js config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '1337',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '1337',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'regops-backend.onrender.com',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
